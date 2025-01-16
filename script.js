5bf86de766ef3efffdb70be6e426329b948e7b47
GaetanTrema <gaetantrema@gmail.com>
2025-01-16 23:26:10 +0100

    test bluetooth

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ function reset() {
 }
 
 const options = {};
-
+alert('Requesting Bluetooth Device...');
 navigator.bluetooth
   .requestDevice(options)
   .then((device) => {
     alert(`Name: ${device.name}`);
     // Do something with the device.
   })
-  .catch((error) => console.error(`Something went wrong. ${error}`));
+  .catch((error) => alert(`Something went wrong. ${error}`));
